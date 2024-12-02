4b90818e56a687533c2a359ec8b8b44583d13369
thejas01 <thejasgs19@gmail.com>
2024-12-02 09:41:20 +0530

    created a repo files

---
+++ src/main/java/com/example/role_based/repository/UserRepository.java
@@ -0,0 +1,19 @@
+package com.example.role_based.repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.stereotype.Repository;
+
+import com.example.role_based.entity.Role;
+import com.example.role_based.entity.User;
+
+@Repository
+@EnableJpaRepositories
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+ Optional<User> findByEmail(String email);
+ Optional<User> findByRole(Role role);
+}
